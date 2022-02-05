b83afbacd378f57ab59a98a9dddcb255cff72fcb
saumya <ksaumyasingh@gmail.com>
2022-02-05 15:08:02 +0530

    initial commit

---
+++ src/airline/management/system/AirlineManagementSystem.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline.management.system;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ksaum
+ */
+public class AirlineManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Scanner o=new Scanner(System.in);
+        Db obj=new Db();
+        obj.dbConnect("Airline Management System");
+       // obj.insertReservation("103","AB566","AJ970","2:00PM","Kolkata","Patna","2021-12-12");
+       //obj.insertFlightSector("AJ970","INDIGO 1","Kolkata","Patna","100","A","First Class");
+       //obj.insertFlightSector("AJ900","INDIGO 2","Kolkata","Patna","550","B","Second Class");
+       //obj.insertFlightSector("AJ960","INDIGO 3","Kolkata","Patna","30","C","Third Class");
+        obj.insertPayment(100,null,"1234987623458765",1200,"2021-11-2","8292560588");
+        obj.insertPayment(103,null,"1234987623456543",1700,"2021-11-21","9123989805");
+        //obj.insertPayment(100,null,"1234987623458765",1200,"2021-11-2","8292560588");
+    }
+    
+}
---
+++ src/airline/management/system/Db.java
@@ -0,0 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline.management.system;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ksaum
+ */
+public class Db {
+    Connection con;
+    public Connection dbConnect(String dbname){
+        try{
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection con=DriverManager.getConnection("jdbc:derby://localhost:1527/"+dbname);
+            System.out.println("connected successfully");
+            this.con=con;
+            return con;
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        } 
+        return null;
+    }
+    public void insert(String id,int p){
+        try{
+            PreparedStatement ps=con.prepareStatement("insert into Login values(?,?)");
+            ps.setString(1,id);
+            ps.setInt(2,p);
+            if(ps.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void insertReservation(String p,String tk,String f,String t,String s,String des,String d){
+        try{
+            PreparedStatement ps=con.prepareStatement("insert into Reservation values(?,?,?,?,?,?,?)");
+            ps.setString(2,tk);
+            ps.setString(1,p);
+            ps.setString(3,f);
+            ps.setString(4,t);
+            ps.setString(7,d);
+            ps.setString(5,s);
+            ps.setString(6,des);
+            if(ps.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void insertPassenger(int p,String tk,String f,String t,String s,String des,String d,String fl){
+        try{
+            PreparedStatement ps=con.prepareStatement("insert into Passenger values(?,?,?,?,?,?,?,?)");
+            ps.setString(2,tk);
+            ps.setInt(1,p);
+            ps.setString(3,f);
+            ps.setString(4,t);
+            ps.setString(7,d);
+            ps.setString(5,s);
+            ps.setString(6,des);
+            ps.setString(8,fl);
+            if(ps.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void insertFlightSector(String p,String tk,String f,String t,String s,String des,String d){
+        try{
+            //PreparedStatement ps=con.prepareStatement("insert into Flight values(?,?,?,?)");
+            PreparedStatement ps1=con.prepareStatement("insert into Sector values(?,?,?,?)");
+            /*ps.setString(2,tk);
+            ps.setString(1,p);
+            ps.setString(3,f);
+            ps.setString(4,t);*/
+            ps1.setString(4,d);
+            ps1.setString(2,s);
+            ps1.setString(3,des);
+            ps1.setString(1,p);
+            if(ps1.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void insertPayment(int p,String ch,String card,int amount,String date,String ph){
+        try{
+            PreparedStatement ps=con.prepareStatement("insert into Payment values(?,?,?,?,?,?)");
+            ps.setInt(1,p);
+            ps.setString(6,ph);
+            ps.setString(2,ch);
+            ps.setString(3,card);
+            ps.setInt(4,amount);
+            ps.setString(5,date);
+            if(ps.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void insertCancellation(int p,String ch,String card,int amount,String date,String ph){
+        try{
+            PreparedStatement ps=con.prepareStatement("insert into Payment values(?,?,?,?,?,?)");
+            ps.setInt(1,p);
+            ps.setString(6,ph);
+            ps.setString(2,ch);
+            ps.setString(3,card);
+            ps.setInt(4,amount);
+            ps.setString(5,date);
+            if(ps.executeUpdate()>0)
+              System.out.println("inserted successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+   /* public void createTable(){
+        try{
+            PreparedStatement ps=con.prepareStatement("create table reservation (pnr_no varchar(10), ticket_id varchar(10), f_code varchar(10), jny_date varchar(20), jny_time varchar(10), src varchar(20), dst varchar(20));
+            if(ps.executeUpdate()>0)
+              System.out.println("created successfully"); 
+            }
+        
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }*/
+    /*public void update(int id,String n,String c,String d){
+        try{
+            PreparedStatement ps=null;
+            ps=con.prepareStatement("update agri set Fname=?,City=?,State=? where FarmerNo=?");
+            ps.setInt(4,id);
+            ps.setString(1,n);
+            ps.setString(2,c);
+            ps.setString(3,d);
+            if(ps.executeUpdate()>0)
+              System.out.println("updated successfully");  
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }*/
+    public ResultSet search(String id,int i)throws Exception{
+        PreparedStatement ps = con.prepareStatement("select * from Login where Username=? AND Password=?");
+        ps.setString(1, id);
+        ps.setInt(2, i);
+        ResultSet rs = ps.executeQuery();
+ 
+        return rs;
+    }
+    public ResultSet searchReservation(String src,String des)throws Exception{
+        PreparedStatement ps = con.prepareStatement("select * from Reservation where src=? AND des =?");
+        ps.setString(1, src);
+        ps.setString(2, des);
+        ResultSet rs = ps.executeQuery();
+ 
+        return rs;
+    }
+    public ResultSet searchFlightInfo(String fl)throws Exception{
+        PreparedStatement ps = con.prepareStatement("select * from Flight where f_code=?");
+        ps.setString(1, fl);
+        ResultSet rs = ps.executeQuery();
+ 
+        return rs;
+    }
+    public ResultSet searchSector(String fl)throws Exception{
+        PreparedStatement ps = con.prepareStatement("select * from Sector where flight_code=?");
+        ps.setString(1, fl);
+        ResultSet rs = ps.executeQuery();
+ 
+        return rs;
+    }
+    public ResultSet searchPayment(int pnr)throws Exception{
+        PreparedStatement ps = con.prepareStatement("select * from Payment where pnr_no=?");
+        ps.setInt(1, pnr);
+        ResultSet rs = ps.executeQuery();
+ 
+        return rs;
+    }
+    
+}
